67d9bfbf63a2b340ffaf59ed7cf4b19d0c8b8cd6
jieluo@google.com <jieluo@google.com@630680e5-0e50-0410-840e-4b1c322b438d>
2014-08-15 00:47:22 +0000

    change 2.5.1-pre to 2.6.0rc1

--- python/setup.py
+++ python/setup.py
@@ -144,7 +144,7 @@ if __name__ == '__main__':
 
 
   setup(name = 'protobuf',
-        version = '2.6-pre',
+        version = '2.6.0rc1',
         packages = [ 'google' ],
         namespace_packages = [ 'google' ],
         google_test_dir = test_dir,
